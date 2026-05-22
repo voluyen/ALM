1ee283d5a6d4c566841482e60dc1dce971c6f9e9
voluyen <voluyen@gmail.com>
2026-05-22 17:20:08 +0700

    Update run script to set CUDA device and adjust model parallelism settings

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@ set -e
 
 NAME=${1:-gpt2_1.5B_cross_tokenizer_distill}
 
-python3 scripts/cross_tokenizer_distill.py \
+CUDA_VISIBLE_DEVICES=0 python3 scripts/cross_tokenizer_distill.py \
     --config=configs/gpt2_1.5B_cross_tokenizer_distill.yaml \
     --overrides \
     n_data_parallel=1 \
-    n_model_parallel=2 \
+    n_model_parallel=1 \
     name=$NAME
